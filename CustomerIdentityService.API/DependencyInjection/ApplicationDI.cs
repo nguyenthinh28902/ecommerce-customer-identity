74085fe958def6bab3ea7ad7732b636505024758
nguyenthinh28902 <nndthinh28902@gmail.com>
2025-12-30 09:50:36 +0700

    Registration DI

---
+++ CustomerIdentityService.API/DependencyInjection/ApplicationDI.cs
@@ -0,0 +1,15 @@
+﻿using CustomerIdentityService.Application.DependencyInjection;
+
+namespace CustomerIdentityService.API.DependencyInjection
+{
+    public static class ApplicationDI
+    {
+        public static IServiceCollection AddApplicationDI(
+            this IServiceCollection services,
+            IConfiguration configuration)
+        {
+            services.AddApplicationServices(configuration);
+            return services;
+        }
+    }
+}
---
+++ CustomerIdentityService.Application/DependencyInjection/ApplicationServiceRegistration.cs
@@ -0,0 +1,23 @@
+﻿using CustomerIdentityService.Infrastructure.DependencyInjection;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomerIdentityService.Application.DependencyInjection
+{
+    public static class ApplicationServiceRegistration
+    {
+        public static IServiceCollection AddApplicationServices(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddInfrastructureServices(configuration);
+
+
+            return services;
+        }
+    }
+}
